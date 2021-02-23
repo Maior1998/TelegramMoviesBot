32c921dac0b57ab0e629292281867e06b9def521
Sergey Patoka <sergey.pat98@gmail.com>
2021-02-23 13:48:12 +0500

    Сущность провайдера данных для бота

---
+++ TelegramMoviesBot/Model/VideoDataProviders/IVideoDataProvider.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using MoviesDatabase.DatabaseModel;
+
+namespace TelegramMoviesBot.Model.VideoDataProviders
+{
+    public interface IVideoDataProvider
+    {
+        public Video[] GetNewVideos();
+    }
+}
---
+++ TelegramMoviesBot/Model/VideoProcessor.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using MoviesDatabase.DatabaseModel;
+using TelegramMoviesBot.Model.VideoDataProviders;
+
+namespace TelegramMoviesBot.Model
+{
+    public class VideoProcessor
+    {
+        private IVideoDataProvider VideoProvider;
+
+        public VideoProcessor(IVideoDataProvider videoProvider)
+        {
+            VideoProvider = videoProvider;
+        }
+
+
+        public void Start()
+        {
+            Video[] newVideos = VideoProvider.GetNewVideos();
+
+        }
+
+    }
+}
