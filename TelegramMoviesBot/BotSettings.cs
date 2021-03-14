9b975806deae1e330c89170b54613bcab0e377e6
Sergey Patoka <sergey.pat98@gmail.com>
2021-03-14 19:13:34 +0500

    Тип считываемой системной переменной сменен на пользотельскую

--- TelegramMoviesBot/BotSettings.cs
+++ TelegramMoviesBot/BotSettings.cs
@@ -15,8 +15,8 @@ namespace TelegramMoviesBot
 
 
         private static string telegramApiKey;
-        public static string TelegramApiKey => telegramApiKey ??= Environment.GetEnvironmentVariable("MY_PP");
+        public static string TelegramApiKey => telegramApiKey ??= Environment.GetEnvironmentVariable("MY_PP", EnvironmentVariableTarget.User);
         private static string movieDbApiKey;
-        public static string MovieDbApiKey => movieDbApiKey ??= Environment.GetEnvironmentVariable("TMDB_API_KEY");
+        public static string MovieDbApiKey => movieDbApiKey ??= Environment.GetEnvironmentVariable("TMDB_API_KEY", EnvironmentVariableTarget.User);
     }
 }
